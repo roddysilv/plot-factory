2f42c2b281580b2d921b75c1a41290a62d15578c
Rodrigo Oliveira Silva <rodrigo.silvoliveira@gmail.com>
2020-09-15 22:49:02 -0300

    Dia 1 - Introdução ao shiny

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(ggplot2)
+library(tidyverse)
 
 bcl <-read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
@@ -8,15 +10,38 @@ ui <- fluidPage(
     sidebarPanel(
       sliderInput("priceInput", "Price", min = 0,max = 100, value = c(25,40), pre = "$"),
       radioButtons("typeInput", "Product type", choices = c("BEER", "REFRESHMENT","SPIRITS","WINE"),selected = "WINE"),
-      
+      selectInput("countryInput","Country", choices = c("CANADA","FRANCE", "ITALY"))
     ),
     mainPanel(
-      "the results will go here"
+      plotOutput("coolplot"),
+      br(),br(),
+      tableOutput("results")
     )
   )
 )
 
 server <- function(input, output, session){
+  filtered <- reactive({
+    req(input$priceInput[1],input$priceInput[2],input$countryInput)
+    bcl %>%
+      filter(
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type == input$typeInput,
+        Country == input$countryInput  
+      )
+  })
+  
+  output$coolplot <- renderPlot({
+    req(filtered)
+        
+    ggplot(filtered(),aes(Alcohol_Content)) + geom_histogram()
+  })
+  
+  output$results <- renderTable({
+    req(filtered)
+    filtered()
+  })
   
 }
 
